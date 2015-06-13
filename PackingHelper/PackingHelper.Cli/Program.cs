35ed3a5acd723f5a1b17abd1bd6754dfff2bde77
jamesology <james@gerstmann.us>
2015-06-13 07:52:54 +0000

    look for settings file

--- PackingHelper/PackingHelper.Cli/Program.cs
+++ PackingHelper/PackingHelper.Cli/Program.cs
@@ -13,7 +13,29 @@ namespace PackingHelper.Cli
             XmlConfigurator.Configure(new FileInfo("PackingHelper.Cli.log4net.config"));
             var log = LogManager.GetLogger("main");
 
-            log.Debug("Log Configured.");
+            try
+            {
+                if (args.Length > 0)
+                {
+                    var settingsFile = args[0];
+                    log.DebugFormat("Settings File: {0}", settingsFile);
+                    log.DebugFormat("File {0}Found", File.Exists(settingsFile) ? String.Empty : "Not ");
+
+                    //populate settings
+                    //process task sets
+                    //write task file
+                }
+                else
+                {
+                    log.Warn("No settings file provided. Unable to proceed.");
+                }
+            }
+            catch(Exception exception)
+            {
+                log.Error("Oh Snap!", exception);
+            }
+
+            Console.Write("Press Enter to exit.");
             Console.ReadLine();
         }
     }
